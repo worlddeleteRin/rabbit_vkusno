03324e42f74b7c062c330ea252b1187143748836
Rin NoName <noname@MacBook-Pro-Rin.lan>
2021-03-08 17:03:02 +0300

    new commit

--- food_fabrik/urls.py
+++ food_fabrik/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', include('products.urls')),
@@ -22,4 +24,4 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('admin/', admin.site.urls),
     path('profile/', include('users.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
