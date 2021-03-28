f6a55c502cec99d78ab402ebf7512379a006956e
Rin NoName <noname@MacBook-Pro-Rin.lan>
2021-03-28 10:53:43 +0300

    new commit

--- food_fabrik/settings.py
+++ food_fabrik/settings.py
@@ -140,7 +140,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 VENV_PATH = os.path.dirname(BASE_DIR)
-STATIC_ROOT = os.path.join(VENV_PATH, 'static_root')
+STATIC_ROOT = os.path.join(VENV_PATH, 'static')
 
 
 # EMAIL_HOST = 'smtp.yandex.ru'
