d7609a3c9d8e89068b1d4d4a166a0b06751f6202
Rin NoName <noname@MacBook-Pro-Rin.lan>
2021-03-28 22:59:14 +0300

    new commit

--- food_fabrik/critical_variables.py
+++ food_fabrik/critical_variables.py
@@ -2,5 +2,5 @@
 DELIVERY_BYCLIENT_DISCOUNT = 10
 DELIVERY_BYCLIENT_ADDRESS = 'г. Алупка, Западная 22-а, павильон на рынке около магазина «Соседи»'
 DELIVERY_PHONE = '79781161415'
-DELIVERY_ADDRESS_MAP = 'https://yandex.ru/maps/27061/alupka/?ll=34.034147%2C44.411945&mode=whatshere&whatshere%5Bpoint%5D=34.031783%2C44.411701&whatshere%5Bzoom%5D=19&z=17'
+DELIVERY_ADDRESS_MAP = 'https://yandex.ru/maps/27061/alupka/?ll=34.031738%2C44.411710&mode=routes&rtext=~44.411711%2C34.031801&rtt=auto&ruri=~&z=18.2'
 CAN_PAY_WITH_BONUS_PERCENT = 25
