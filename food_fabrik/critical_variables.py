e74cedd38bc8ba36b122ecd417d061738a865007
Rin NoName <noname@MacBook-Pro-Rin.lan>
2021-04-07 23:00:02 +0300

    new commit

--- food_fabrik/critical_variables.py
+++ food_fabrik/critical_variables.py
@@ -1,13 +1,8 @@
 # custom variables
 DELIVERY_BYCLIENT_DISCOUNT = 10
-# DELIVERY_BYCLIENT_ADDRESS = 'г. Алупка, Западная 22-а, павильон на рынке около магазина «Соседи»'
-# DELIVERY_BYCLIENT_ADDRESS = 'г. Москва, Яузский бульвар, 9/6с5'
-DELIVERY_BYCLIENT_ADDRESS = '-'
-# DELIVERY_MAIN_INFO = 'Доставка по г. Москва - бесплатно'
-DELIVERY_MAIN_INFO = '-'
-# DELIVERY_PHONE = '79781161415'
-DELIVERY_PHONE = '74951161415'
-# DELIVERY_ADDRESS_MAP = 'https://yandex.ru/maps/27061/alupka/?ll=34.031738%2C44.411710&mode=whatshere&rtext=~44.411711%2C34.031801&rtt=auto&ruri=~&whatshere%5Bpoint%5D=34.031531%2C44.411926&whatshere%5Bzoom%5D=18.2&z=18.2'
-# DELIVERY_ADDRESS_MAP = 'https://yandex.ru/maps/geo/moskva/53000094/?ll=37.385524%2C55.584222&z=9.11'
-DELIVERY_ADDRESS_MAP = 'https://google.com'
+DELIVERY_BYCLIENT_ADDRESS = 'г. Краснодар, ул. Монтажников, 12.'
+DELIVERY_MAIN_INFO = 'Доставка осуществляется по г. Краснодар'
+# DELIVERY_PHONE = '78611511254'
+DELIVERY_PHONE = '78611511254'
+DELIVERY_ADDRESS_MAP = 'https://yandex.ru/maps/35/krasnodar/house/ulitsa_montazhnikov_12/Z0EYfwFmQUcDQFpvfXx0eHlqbQ==/?ll=38.966824%2C45.059600&z=16'
 CAN_PAY_WITH_BONUS_PERCENT = 25
